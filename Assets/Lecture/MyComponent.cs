2f0ec3c4dec1add95cf66ebbf5483e96a16b64a9
zeromemory00 <zeromemory00@gmail.com>
2016-08-31 12:30:17 +0900

    1st Lecture (16/08/31)
    GitHub Test & Simple Component Create

---
+++ Assets/Lecture/MyComponent.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyComponent : MonoBehaviour {
+
+    public int intVariable;
+    public float floatVariable;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public int IntVar //Property
+    {
+        get // read Only
+        {
+            return intVariable;
+        }
+        set // Write Only
+        {
+            intVariable = value;
+        }
+    }
+
+    public void DoSomething()
+    {
+        intVariable++;
+    }
+}

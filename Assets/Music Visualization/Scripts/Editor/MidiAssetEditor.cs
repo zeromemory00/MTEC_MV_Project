7c16491e32df89f0528d2406275fd199ba7342f8
zeromemory00 <zeromemory00@gmail.com>
2016-11-23 06:23:33 +0900

    5th Lecture (16.10.05)
    DrawTrackListArea 구현
    DrawNoteArea 구현
    DrawMusicalScaleArea 구현중

--- Assets/Music Visualization/Scripts/Editor/MidiAssetEditor.cs
+++ Assets/Music Visualization/Scripts/Editor/MidiAssetEditor.cs
@@ -23,18 +23,26 @@ public class MidiAssetEditor : Editor
 		_foldout = EditorGUILayout.Foldout(_foldout, "Time Signiture");
 		if(_foldout == true)
 		{
-			EditorGUI.indentLevel++;
-			EditorGUILayout.LabelField(string.Format("PPQN: {0:d}", midiAsset.PPQN));
-			EditorGUILayout.LabelField(string.Format("Pulse: {0:f} sec", midiAsset.pulseTime));
-			EditorGUILayout.LabelField(string.Format("BPM: {0:d}", midiAsset.BPM));
-			EditorGUILayout.LabelField(string.Format("Nemerator: {0:d}", midiAsset.numerator));
-			EditorGUILayout.LabelField(string.Format("Denominator: {0:d}", midiAsset.denominator));
-			EditorGUI.indentLevel--;
+            //EditorGUI.indentLevel++;
+            //EditorGUILayout.LabelField(string.Format("PPQN: {0:d}", midiAsset.PPQN));
+            //EditorGUILayout.LabelField(string.Format("Pulse: {0:f} sec", midiAsset.pulseTime));
+            //EditorGUILayout.LabelField(string.Format("BPM: {0:d}", midiAsset.BPM));
+            //EditorGUILayout.LabelField(string.Format("Nemerator: {0:d}", midiAsset.numerator));
+            //EditorGUILayout.LabelField(string.Format("Denominator: {0:d}", midiAsset.denominator));
+
+            EditorGUILayout.LabelField(string.Format("PPQN: {0:d}", midiAsset.PPQN));
+            EditorGUILayout.LabelField(string.Format("Pulse: {0:f} sec", midiAsset.pulseTime));
+            EditorGUILayout.LabelField(string.Format("BPM: {0:d}", midiAsset.BPM));
+            EditorGUILayout.LabelField(string.Format("Nemerator: {0:d}", midiAsset.numerator));
+            EditorGUILayout.LabelField(string.Format("Denominator: {0:d}", midiAsset.denominator));
+            
+            EditorGUI.indentLevel--;
 		}
 
 		if(GUILayout.Button("Track Viewer") == true)
 		{
-			MidiTrackWindow.ShowWindow();
+//			MidiTrackWindow.ShowWindow();
+            MidiTrackWindow.ShowWindow(midiAsset);
 		}
 	}
 }
---
+++ Assets/Music Visualization/Scripts/Editor/MidiPlayerEditor.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(MidiPlayer))]
+public class MidiPlayerEditor : Editor
+{
+
+    SerializedProperty midi;
+    SerializedProperty music;
+    SerializedProperty audioSource;
+    SerializedProperty playDelayTime;
+    SerializedProperty playSpeed;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        midi = serializedObject.FindProperty("midi");
+        music = serializedObject.FindProperty("music");
+        audioSource = serializedObject.FindProperty("audioSource");
+        playDelayTime = serializedObject.FindProperty("playDelayTime");
+        playSpeed = serializedObject.FindProperty("playSpeed");
+    }
+
+    // Update is called once per frame
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        EditorGUILayout.PropertyField(midi);
+        EditorGUILayout.PropertyField(music);
+        EditorGUILayout.PropertyField(audioSource);
+        EditorGUILayout.PropertyField(playDelayTime);
+        EditorGUILayout.PropertyField(playSpeed);
+
+        serializedObject.ApplyModifiedProperties();
+
+        MidiPlayer midiplayer = (MidiPlayer)target;
+
+        if (Application.isPlaying == true)
+        {
+            GUILayout.BeginHorizontal();
+            if (midiplayer.IsPlaying == true)
+            {
+                if (GUILayout.Button("Pause") == true)
+                {
+                    midiplayer.Pause();
+                }
+
+
+            }
+            else
+            {
+                if (midiplayer.playTime == 0)
+                {
+                    if (GUILayout.Button("Play") == true)
+                    {
+                        midiplayer.Play();
+                    }
+                }
+                else
+                {
+                    if (GUILayout.Button("Resume") == true)
+                    {
+                        midiplayer.Resume();
+                    }
+                }
+            }
+
+            if (GUILayout.Button("Stop") == true)
+            {
+                midiplayer.Stop();
+            }
+            GUILayout.EndHorizontal();
+
+            EditorGUILayout.Slider(midiplayer.playTime, 0f, midiplayer.totalTime);
+            EditorGUILayout.LabelField(string.Format("Time : {0:f1}sec", midiplayer.playTime));
+        }
+    }
+}
